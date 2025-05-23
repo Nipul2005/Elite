3bfaead8d1b14802e1a0b70e3b32eb18a82f6dc9
NipulPanit <pnipul7678@gmail.com>
2025-05-23 15:38:37 +0530

    finally all  pages now in dist

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        about: path.resolve(__dirname, "about.html"),
+        contact: path.resolve(__dirname, "contact.html"),
+        courses: path.resolve(__dirname, "courses.html"),
+      },
+    },
+  },
 });
