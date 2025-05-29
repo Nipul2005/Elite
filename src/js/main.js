0c694da2d2a7eb4ac22df5d04045d6062eaf1638
NipulPanit <pnipul7678@gmail.com>
2025-05-29 23:35:24 +0530

    for seo chqanges in home pagge content, also added cta at bottom

---
+++ src/js/main.js
@@ -0,0 +1,60 @@
+const featureBoxes = document.getElementById("featureBox");
+const leftArrow = document.getElementById("leftArrow");
+const rightArrow = document.getElementById("rightArrow");
+const showMore = document.getElementById("showMore");
+const cards = document.querySelectorAll(".cards");
+
+let currentIndx = 0;
+let cardsPerSlide;
+let totalCards = 12;
+let maxIdx;
+
+showMore.addEventListener("click", () => {
+  cards.forEach((card) => {
+    card.classList.remove("hidden");
+    card.classList.add("flex");
+  });
+
+  showMore.classList.add("hidden");
+});
+
+function updateCrousel() {
+  const translateX = -currentIndx * 100;
+  featureBoxes.style.transform = `translateX(${translateX}%)`;
+}
+// will add window resize s if soomeone check the feature box in inspect and move mobile view to desktop so cards set itself automatically
+
+function nextSlide() {
+  cardsPerSlide =
+    window.innerWidth < 640 ? (cardsPerSlide = 1) : (cardsPerSlide = 3);
+
+  if (currentIndx > 0) {
+    leftArrow.classList.add("opacity-100");
+  }
+
+  if (currentIndx < totalCards / cardsPerSlide - 1) {
+    currentIndx = currentIndx + 1;
+    updateCrousel();
+  }
+
+  if (currentIndx == totalCards / cardsPerSlide - 1) {
+    rightArrow.classList.add("opacity-0");
+  }
+}
+
+function prevSlide() {
+  if (currentIndx == 1) {
+    leftArrow.classList.remove("opacity-100");
+  }
+
+  if (currentIndx < totalCards / cardsPerSlide - 1) {
+    rightArrow.classList.remove("opacity-0");
+  }
+  if (currentIndx > 0) {
+    currentIndx = currentIndx - 1;
+    updateCrousel();
+  }
+}
+
+rightArrow.addEventListener("click", nextSlide);
+leftArrow.addEventListener("click", prevSlide);
