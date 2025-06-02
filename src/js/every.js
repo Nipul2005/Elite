57d3c7e6fd80d9b46d79eaae4ad508d9db307f8d
NipulPanit <pnipul7678@gmail.com>
2025-06-02 15:24:44 +0530

    done js edding

--- src/js/every.js
+++ src/js/every.js
@@ -1,8 +1,8 @@
 const sidebar = document.getElementById("sidebar");
 const open = document.getElementById("open");
 const close = document.getElementById("close");
-const floater= document.getElementById("floater");
-
+const floater = document.getElementById("floater");
+let flag = true;
 
 function openSidebar() {
   sidebar.classList.remove("top-1000");
@@ -10,15 +10,21 @@ function openSidebar() {
 }
 
 function closeSidebar() {
-    sidebar.classList.add("-top-1000");
-    sidebar.classList.remove("top-0");
+  sidebar.classList.add("-top-1000");
+  sidebar.classList.remove("top-0");
 }
 
 open.addEventListener("click", openSidebar);
 close.addEventListener("click", closeSidebar);
 
 floater.addEventListener("click", (e) => {
-    floater.classList.toggle("-right-25");
+  if(flag){
+    floater.classList.add("right-0");
+    floater.classList.remove("-right-25");
+    flag = false;
+  }else{
+    floater.classList.add("-right-25");
+    floater.classList.remove("right-0");
+    flag = true;
+  }
 });
-
-
