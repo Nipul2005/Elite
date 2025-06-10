ac0deee8a28a8704d83f914fc0aeeec108f51bcf
NipulPanit <pnipul7678@gmail.com>
2025-06-10 13:55:55 +0530

    chekcing

--- src/js/form.js
+++ src/js/form.js
@@ -1,3 +1,12 @@
+import emailjs from "@emailjs/browser"
+
+(function () {
+  // https://dashboard.emailjs.com/admin/account
+  emailjs.init({
+    publicKey: "0JXto0xokULVJrV8_",
+  });
+})();
+
 const form=document.getElementById("form");
 
 form.addEventListener("submit",function(e){
