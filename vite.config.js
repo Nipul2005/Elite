eb31522b31c6072537d03d18c9a2af9c1b81c33d
NipulPanit <pnipul7678@gmail.com>
2025-06-25 15:12:46 +0530

    test5

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,50 @@ export default defineConfig({
         main: resolve(__dirname, "index.html"),
         about: resolve(__dirname, "about.html"),
         contact: resolve(__dirname, "contact.html"),
-        courses: resolve(__dirname, "courses.html"),
+        "courses/Diploma-Computer-Teacher-Training-in-delhi": resolve(
+          __dirname,
+          "courses/dctt.html"
+        ),
+        "courses/Diploma-E-accounting-and-Taxation-in-delhi": resolve(
+          __dirname,
+          "courses/det.html"
+        ),
+        "courses/Diploma-Computer-Application-in-delhi": resolve(
+          __dirname,
+          "courses/dca.html"
+        ),
+        "courses/Diploma-Graphic-Designing-in-delhi": resolve(
+          __dirname,
+          "courses/dgd.html"
+        ),
+        "courses/Diploma-Web-Designing-Multimedia-in-delhi": resolve(
+          __dirname,
+          "courses/dwd.html"
+        ),
+        "courses/Certification-Office-Automation-in-delhi": resolve(
+          __dirname,
+          "courses/coa.html"
+        ),
+        "courses/Certification-Desktop-Publishing-in-delhi": resolve(
+          __dirname,
+          "courses/dtp.html"
+        ),
+        "courses/Certification-Computer-Operating-in-delhi": resolve(
+          __dirname,
+          "courses/cco.html"
+        ),
+        "courses/Certification-Video-Editing-in-delhi": resolve(
+          __dirname,
+          "courses/cve.html"
+        ),
+        "courses/Certification-Financial-Accounting-in-delhi": resolve(
+          __dirname,
+          "courses/cfa.html"
+        ),
+        "courses/Management-Information-System-in-delhi": resolve(
+          __dirname,
+          "courses/mis.html"
+        ),
       },
     },
   },
