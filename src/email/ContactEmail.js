0866a087e3563f8ea1b0a0e779df0ce7e9f72efe
NipulPanit <pnipul7678@gmail.com>
2025-07-14 16:18:07 +0530

    done in contact home page carousel remain

--- src/email/ContactEmail.js
+++ src/email/ContactEmail.js
@@ -1,7 +1,9 @@
 import emailjs from "@emailjs/browser";
 
 const btn = document.getElementById("btn");
+const dialog = document.getElementById("dialog");
 
+// emailjs publiuc key
 (function () {
   // https://dashboard.emailjs.com/admin/account
   emailjs.init({
@@ -10,6 +12,7 @@ const btn = document.getElementById("btn");
 })();
 
 const form = document.getElementById("form");
+const errMessage = `<h2 class="text-3xl">Thanks for contacting us</h2><p class="text-lg text-red-300 text-center">However, we encountered an error sending your message.</p>`;
 
 form.addEventListener("submit", function (e) {
   btn.innerHTML = "Sending...";
@@ -18,11 +21,27 @@ form.addEventListener("submit", function (e) {
     () => {
       alert("Message sent successfully!");
       btn.innerHTML = "Submit";
+      dialog.classList.remove("-top-100", "opacity-0");
+      dialog.classList.add("top-0", "opacity-100");
+
+      setTimeout(() => {
+        dialog.classList.remove("top-0", "opacity-100");
+        dialog.classList.add("-top-100", "opacity-0");
+      }, 3000);
+
       this.reset();
     },
     (err) => {
       alert("Failed to send message.");
       btn.innerHTML = "Submit";
+      dialog.innerHTML = errMessage;
+      dialog.classList.remove("-top-100", "opacity-0");
+      dialog.classList.add("top-0", "opacity-100");
+
+      setTimeout(() => {
+        dialog.classList.remove("top-0", "opacity-100");
+        dialog.classList.add("-top-100", "opacity-0");
+      }, 4000);
     }
   );
 });
